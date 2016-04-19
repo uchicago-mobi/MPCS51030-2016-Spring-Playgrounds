9f5e17e8cd89052fed1c8c63ae670b1fe0eb5a95
T. Andrew Binkowski <abinkowski@uchicago.edu>
2016-04-19 17:38:57 -0500

    Add some more comments to explain what's going on with the star view controller.

--- 2016-ShareExtension/FavoriteExtension/ShareViewController.swift
+++ 2016-ShareExtension/FavoriteExtension/ShareViewController.swift
@@ -11,111 +11,117 @@ import Social
 import ExtensionKit
 import MobileCoreServices
 
+/// View Controller that is presented via the share extension.  This uses the 
+/// default share sheet from the `Social.framework`.
 class ShareViewController: SLComposeServiceViewController {
-
-    var imageToShare: UIImage?
-    var urlToShare: String?
-    var textToShare: String?
   
-    ///
-    ///
-    ///
-    override func presentationAnimationDidFinish() {
-        let items = extensionContext?.inputItems
-        var itemProvider: NSItemProvider?
-        
-        // Exit early if there are no items
-        if items == nil && items!.isEmpty != false { return }
-        let item = items![0] as! NSExtensionItem
-        print("Attachment items: \(items)")
-        
-        // Loop through all the attachment items and store their values in properties
+  /// Hold the app icon
+  var imageToShare: UIImage?
+  /// Hold the app store URL
+  var urlToShare: String?
+  /// Hold the application title
+  var textToShare: String?
+  
+
+  //
+  // MARK: - Share extnesion methods
+  //
+  override func presentationAnimationDidFinish() {
+    let items = extensionContext?.inputItems
+    var itemProvider: NSItemProvider?
+    
+    // Exit early if there are no items
+    if items == nil && items!.isEmpty != false { return }
+    let item = items![0] as! NSExtensionItem
+    print("Attachment items: \(items)")
+    
+    // Loop through all the attachment items and store their values in properties
+    
+    // Note:
+    // Note: The App Store makes three attachements available: image, plain-text, and url
+    // Note:
+    
+    if let attachments = item.attachments {
       
-        // Note:
-        // Note: The App Store makes three attachements available: image, plain-text, and url
-        // Note:
-      
-        if let attachments = item.attachments {
-
-            for attachment in attachments {
-                itemProvider = attachment as? NSItemProvider
-
-                // Get text
-                let textType = kUTTypePlainText as String
-                if itemProvider?.hasItemConformingToTypeIdentifier(textType) == true {
-                    itemProvider?.loadItemForTypeIdentifier(textType, options: nil) { (item, error) -> Void in
-                        if error == nil {
-                            if let text = item as? String {
-                                self.textToShare = text
-                            }
-                        }
-                    }
-                }
-
-                // Get URL
-                let urlType = kUTTypeURL as String
-                if itemProvider?.hasItemConformingToTypeIdentifier(urlType) == true {
-                    itemProvider?.loadItemForTypeIdentifier(urlType, options: nil) { (item, error) -> Void in
-                        if error == nil {
-                            if let url = item as? NSURL {
-                                self.urlToShare = url.absoluteString
-                            }
-                        }
-                    }
-                    
-                }
-                
-                // Get image
-                let imageType = kUTTypeImage as String
-                if itemProvider?.hasItemConformingToTypeIdentifier(imageType) == true {
-                    itemProvider?.loadItemForTypeIdentifier(imageType, options: nil) { (item, error) -> Void in
-                        if error == nil {
-                            print("Item: \(item)")
-                            if let image = item as? UIImage {
-                                self.imageToShare = image
-                            }
-                        }
-                    }
-                }
-                
+      for attachment in attachments {
+        itemProvider = attachment as? NSItemProvider
+        
+        // Get text
+        let textType = kUTTypePlainText as String
+        if itemProvider?.hasItemConformingToTypeIdentifier(textType) == true {
+          itemProvider?.loadItemForTypeIdentifier(textType, options: nil) { (item, error) -> Void in
+            if error == nil {
+              if let text = item as? String {
+                self.textToShare = text
+              }
             }
+          }
         }
         
+        // Get URL
+        let urlType = kUTTypeURL as String
+        if itemProvider?.hasItemConformingToTypeIdentifier(urlType) == true {
+          itemProvider?.loadItemForTypeIdentifier(urlType, options: nil) { (item, error) -> Void in
+            if error == nil {
+              if let url = item as? NSURL {
+                self.urlToShare = url.absoluteString
+              }
+            }
+          }
+          
+        }
+        
+        // Get image
+        let imageType = kUTTypeImage as String
+        if itemProvider?.hasItemConformingToTypeIdentifier(imageType) == true {
+          itemProvider?.loadItemForTypeIdentifier(imageType, options: nil) { (item, error) -> Void in
+            if error == nil {
+              print("Item: \(item)")
+              if let image = item as? UIImage {
+                self.imageToShare = image
+              }
+            }
+          }
+        }
+        
+      }
     }
     
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
-    }
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of
-        // contentText and/or NSExtensionContext attachments.
-        
-        // While it is possible to save an UIImage to a dictionary as NSData as
-        // we are doing here, it is generally not a good idea.  You could store
-        // image on server or in App Group folder and keep the URL of the location
-        // to store
-
-        // Also, note that you get the app store URL, so you could use it to get
-        // the image that way.
-        let imageData: NSData? = UIImagePNGRepresentation(imageToShare!)
-        let sharedItem: NSDictionary = [ "date": NSDate(),
-                                        "contentText": textToShare!,
-                                        "imageData": imageData!,
-                                        "url": urlToShare!
-                                        ]
-        
-        LocalDefaultsManager.sharedInstance.add(object: sharedItem)
-        
-        
-        // Inform the host that we're done, so it un-blocks its UI.
-        // Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
-    }
-
-    override func configurationItems() -> [AnyObject]! {
-        // To add configuration options via table cells at the bottom of the sheet,
-        // return an array of SLComposeSheetConfigurationItem here.
-        return []
-    }
+  }
+  
+  override func isContentValid() -> Bool {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return true
+  }
+  override func didSelectPost() {
+    // This is called after the user selects Post. Do the upload of
+    // contentText and/or NSExtensionContext attachments.
+    
+    // While it is possible to save an UIImage to a dictionary as NSData as
+    // we are doing here, it is generally not a good idea.  You could store
+    // image on server or in App Group folder and keep the URL of the location
+    // to store
+    
+    // Also, note that you get the app store URL, so you could use it to get
+    // the image that way.
+    let imageData: NSData? = UIImagePNGRepresentation(imageToShare!)
+    let sharedItem: NSDictionary = [ "date": NSDate(),
+                                     "contentText": textToShare!,
+                                     "imageData": imageData!,
+                                     "url": urlToShare!
+    ]
+    
+    LocalDefaultsManager.sharedInstance.add(object: sharedItem)
+    
+    
+    // Inform the host that we're done, so it un-blocks its UI.
+    // Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+  }
+  
+  override func configurationItems() -> [AnyObject]! {
+    // To add configuration options via table cells at the bottom of the sheet,
+    // return an array of SLComposeSheetConfigurationItem here.
+    return []
+  }
 }
